5f4aa8dce104f8825453a10566ccacb4cd4afa3c
dmunozs <munoz_adrian@hotmail.com>
2024-09-11 17:59:26 -0600

    Almacenamiento de usuarios usando jdbc

--- src/main/java/tacos/config/SecurityConfig.java
+++ src/main/java/tacos/config/SecurityConfig.java
@@ -5,7 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-/*import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
@@ -13,13 +14,14 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;*/
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
-//@Configuration
+@Configuration
 public class SecurityConfig  {
-/*
+
     @Bean
     public DataSource dataSource(){
         return new EmbeddedDatabaseBuilder()
@@ -37,12 +39,24 @@ public class SecurityConfig  {
                 .roles("USER")
                 .build();
 
+
         JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
         users.createUser(user);
 
         return users;
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                        (req)-> req.requestMatchers("/h2-console/**").permitAll()
+                                .requestMatchers("/design/**").permitAll()
+                                .anyRequest().authenticated())
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
+                .headers( headers-> headers.frameOptions(frameOptionsConfig -> frameOptionsConfig.disable()));  // Desactivar protección de frames para permitir la consola de H2
 
- */
+        return http.build();
+    }
+
+
 }
